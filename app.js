c62e3d9ed3cefef4e0ab0f0a16176a7c86cf6a10
Abderaouf16 <abderaoufa88@gmail.comm>
2023-08-29 14:15:17 +0100

    printing all input value in the design card only the cvc

--- app.js
+++ app.js
@@ -5,145 +5,206 @@ const expYear = document.getElementById("year");
 const cvc = document.getElementById("cvc-num");
 const printedCardNumber = document.getElementById("printed-CardNumber");
 const printedOwnerName = document.getElementById("printed-owner");
-
-
-
+const printedExpMount = document.getElementById("printed-month");
+const printedExpYear = document.getElementById("printed-year");
 const from = document.getElementById("form");
+
+// FOR NAME INPUT
 
 cardOwner.addEventListener("input", function cardNameValidation() {
   inputFieldVlue = cardOwner.value;
   const onlyLettersRegex = /^[A-Za-z]+$/;
 
-  if (cardOwner.value == "") {
-    cardOwner.style.borderColor = "red";
-    printedOwnerName.textContent = ''
-    setErrorMessage(1,"Can't be blank.")
-  }else if (!onlyLettersRegex.test(inputFieldVlue)) {
-    cardOwner.style.borderColor = "red";
-    setErrorMessage(1,"Wrong format")
-  } else {
-    cardOwner.style.borderColor = "green";
-    printedOwnerName.textContent = inputFieldVlue;
-    setSuccess(1)
+  switch (true) {
+    case cardOwner.value == "":
+      cardOwner.style.borderColor = "red";
+      printedOwnerName.textContent = "";
+      setErrorMessage(1, "Can't be blank.");
+      break;
+    case !onlyLettersRegex.test(inputFieldVlue):
+      cardOwner.style.borderColor = "red";
+      setErrorMessage(1, "Wrong format");
+      break;
+    case cardOwner.value.length > 16:
+      cardOwner.value = cardOwner.value.slice(0, 16);
+      break;
+    default:
+      cardOwner.style.borderColor = "green";
+      printedOwnerName.textContent = inputFieldVlue;
+      setSuccess(1);
   }
 });
+
+// FOR CARD NUMBER INPUT
 
 cardNumber.addEventListener("input", function cardNumberValidation() {
-  inputFieldVlue = cardNumber.value;
+  const inputFieldValue = cardNumber.value;
   const onlyNumbersRegex = /^\d+$/;
-  if (cardNumber.value === "") {
-    cardNumber.style.borderColor = "red";
-    printedCardNumber.innerText = "0000 0000 0000 0000";
-    setErrorMessage(2,"Can't be blank.")
 
-  }  else if (!onlyNumbersRegex.test(cardNumber.value)) {
-    cardNumber.style.borderColor = "red";
-    setErrorMessage(2,"Wrong format, numbers only.")
-  }else if (cardNumber.value.length > 15) {
-    cardNumber.value= cardNumber.value.slice(0,16)
-    cardNumber.style.borderColor = "green";
-    setSuccess(2)
+  switch (true) {
+    case inputFieldValue === "":
+      cardNumber.style.borderColor = "red";
+      printedCardNumber.textContent = "0000 0000 0000 0000";
+      setErrorMessage(2, "Can't be blank.");
+      break;
 
-}  else if (cardNumber.value.length < 16) {
-    cardNumber.style.borderColor = "red";
-    setErrorMessage(2,"Must enter 16 number")
-  } else {
-    cardNumber.style.borderColor = "green";
-    printedCardNumber.innerText = formatNumber(inputFieldVlue, 16);
-    setSuccess(2)
+    case !onlyNumbersRegex.test(inputFieldValue):
+      cardNumber.style.borderColor = "red";
+      setErrorMessage(2, "Wrong format, numbers only.");
+      break;
+
+    case onlyNumbersRegex.test(inputFieldValue):
+      if (inputFieldValue.length > 15) {
+        cardNumber.value = inputFieldValue.slice(0, 16);
+        cardNumber.style.borderColor = "green";
+        setSuccess(2);
+      } else if (inputFieldValue.length < 16) {
+        cardNumber.style.borderColor = "red";
+        setErrorMessage(2, "Must enter 16 numbers.");
+      } else {
+        cardNumber.style.borderColor = "green";
+        setSuccess(2);
+      }
+      printedCardNumber.innerText = formatNumber(inputFieldValue, 16);
+      break;
+
+    default:
+      cardNumber.style.borderColor = "green";
+      setSuccess(2);
+      break;
   }
 });
+
+// FOR MONTH INPUT
 
 expMonth.addEventListener("input", function expMonthValidation() {
   const onlyNumbersRegex = /^\d+$/;
-  if (expMonth.value == "") {
-    expMonth.style.borderColor = "red";
-    setErrorMessage(3,"Can't be blank.")
 
-  } else if (!onlyNumbersRegex.test(expMonth.value)) {
-    expMonth.style.borderColor = "red";
-    setErrorMessage(3,"Wrong format, numbers only.")
-  }else if (expMonth.value.length > 2) {
-    expMonth.value= expMonth.value.slice(0,2)
-} else if (expMonth.value > 12) {
-    expMonth.style.borderColor = "red";
-    setErrorMessage(3,"Only 12 months in a year")
-  }   else {
-    expMonth.style.borderColor = "green";
-    setSuccess(3)
+  switch (true) {
+    case expMonth.value == "":
+      expMonth.style.borderColor = "red";
+      setErrorMessage(3, "Can't be blank.");
+      printedExpMount.textContent = "00";
+      break;
+    case !onlyNumbersRegex.test(expMonth.value):
+      expMonth.style.borderColor = "red";
+      setErrorMessage(3, "Wrong format, numbers only.");
+      break;
+    case onlyNumbersRegex.test(expMonth.value):
+      if (expMonth.value.length > 2) {
+        expMonth.value = expMonth.value.slice(0, 2);
+      } else if (expMonth.value > 12) {
+        expMonth.style.borderColor = "red";
+        setErrorMessage(3, "Only 12 months in a year");
+      }
+      printedExpMount.innerText = formatNumber(expMonth.value, 2);
+
+      break;
+    default:
+      expMonth.style.borderColor = "green";
+      setSuccess(3);
   }
 });
+
+// FOR YEAR INPUT
 
 expYear.addEventListener("input", function expMonthValidation() {
   const onlyNumbersRegex = /^\d+$/;
-  if (expYear.value == "") {
-    expYear.style.borderColor = "red";
-    setErrorMessage(3,"Can't be blank.")
 
-  }else if (!onlyNumbersRegex.test(expYear.value)) {
-    expYear.style.borderColor = "red";
-    setErrorMessage(3,"Wrong format, numbers only.")
-  }else if (expYear.value.length > 2) {
-    expYear.value= expYear.value.slice(0,2)
-}  else {
-    expYear.style.borderColor = "green";
-    setSuccess(3)
+  switch (true) {
+    case expYear.value == "":
+      expYear.style.borderColor = "red";
+      setErrorMessage(3, "Can't be blank.");
+      printedExpYear.textContent = "00";
+      break;
+    case !onlyNumbersRegex.test(expYear.value):
+      expYear.style.borderColor = "red";
+      setErrorMessage(3, "Wrong format, numbers only.");
+      break;
+    case onlyNumbersRegex.test(expYear.value):
+      if (expYear.value.length > 2) {
+        expYear.value = expYear.value.slice(0, 2);
+      }
+      printedExpYear.innerText = formatNumber(expYear.value, 2);
+      break;
+    default:
+      expYear.style.borderColor = "green";
+      setSuccess(3);
   }
 });
+
+//FOR CVC INPUT
 
 cvc.addEventListener("input", function expMonthValidation() {
   const onlyNumbersRegex = /^\d+$/;
-  if (cvc.value == "") {
-    cvc.style.borderColor = "red";
-    setErrorMessage(4,"Can't be blank.")
 
-  }else if (!onlyNumbersRegex.test(cvc.value)) {
-    cvc.style.borderColor = "red";
-    setErrorMessage(4,"Wrong format, numbers only.")
-  }
-  else if (cvc.value.length > 3) {
-        cvc.value= cvc.value.slice(0,3)
-  } else {
-    cvc.style.borderColor = "green";
-    setSuccess(4)
+  switch (true) {
+    case cvc.value == "":
+      cvc.style.borderColor = "red";
+      setErrorMessage(4, "Can't be blank.");
+      break;
+    case !onlyNumbersRegex.test(cvc.value):
+      cvc.style.borderColor = "red";
+      setErrorMessage(4, "Wrong format, numbers only.");
+      break;
+    case cvc.value.length > 3:
+      cvc.value = cvc.value.slice(0, 3);
+      break;
+    default:
+      cvc.style.borderColor = "green";
+      setSuccess(4);
   }
 });
 
-function setErrorMessage (num,message){
- 
-    if(num==1){
+// FOR DISPLAYING THE ERROR MESSAGE
+
+function setErrorMessage(num, message) {
+  switch (num) {
+    case 1:
       const cardOwnerMessage = document.getElementById("1");
       cardOwnerMessage.innerText = message;
-    }if(num==2){
+      break;
+    case 2:
       const cardNumberMessage = document.getElementById("2");
       cardNumberMessage.innerText = message;
-    }if(num==3){
+      break;
+    case 3:
       const expMonthMessage = document.getElementById("3");
       expMonthMessage.innerText = message;
-
-    }if(num==4){
+      break;
+    case 4:
       const expYearMessage = document.getElementById("4");
       expYearMessage.innerText = message;
-
-    }
+  }
 }
 
- function setSuccess (num){
- 
+// FOR THE VALIDATION SECCESS 
 
-  if(num==1){
-    const cardOwnerMessage = document.getElementById("1");
-    cardOwnerMessage.innerText = '';
-  }if(num==2){
-    const cardNumberMessage = document.getElementById("2");
-    cardNumberMessage.innerText = '';
-  }if(num==3){
-    const experationMessage = document.getElementById("3");
-    experationMessage.innerText = '';
-
-  }if(num==4){
-    const cvcMesssage = document.getElementById("4");
-    cvcMesssage.innerText = '';
-
+function setSuccess(num) {
+  switch (num) {
+    case 1:
+      const cardOwnerMessage = document.getElementById("1");
+      cardOwnerMessage.innerText = "";
+      break;
+    case 2:
+      const cardNumberMessage = document.getElementById("2");
+      cardNumberMessage.innerText = "";
+      break;
+    case 3:
+      const expMonthMessage = document.getElementById("3");
+      experationMessage.innerText = "";
+      break;
+    case 4:
+      const expYearMessage = document.getElementById("4");
+      expYearMessage.innerText = "";
   }
- }
+}
+
+function formatNumber(str, len) {
+  str = str + "0".repeat(len - str.length);
+  let arr = [];
+  for (let i = 0; i <= str.length; i += 4) {
+    arr.push(str.slice(i, i + 4));
+  }
+  return arr.join(" ");
+}
