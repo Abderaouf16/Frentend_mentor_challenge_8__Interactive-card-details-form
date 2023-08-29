e5cc1900c112753dc1de257f5ac679d42080a82e
Abderaouf16 <abderaoufa88@gmail.comm>
2023-08-29 18:38:41 +0100

    challenge finished

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const printedCardNumber = document.getElementById("printed-CardNumber");
 const printedOwnerName = document.getElementById("printed-owner");
 const printedExpMount = document.getElementById("printed-month");
 const printedExpYear = document.getElementById("printed-year");
+const printedCvc = document.getElementById("printed-cvc");
+
 const from = document.getElementById("form");
-
-
-
 
 // FOR DISPLAYING THE ERROR MESSAGE
 
@@ -98,12 +97,7 @@ function greenBorder(input) {
   }
 }
 
-
-
-
-
-
-// FOR NAME INPUT 
+// FOR NAME INPUT
 
 cardOwner.addEventListener("input", function cardNameValidation() {
   inputFieldVlue = cardOwner.value;
@@ -190,7 +184,7 @@ expMonth.addEventListener("input", function expMonthValidation() {
     case onlyNumbersRegex.test(expMonth.value):
       if (expMonth.value.length > 2) {
         expMonth.value = expMonth.value.slice(0, 2);
-      }else if (expMonth.value.length < 2) {
+      } else if (expMonth.value.length < 2) {
         setErrorMessage(3, "must entre two numbers");
         redBorder(expMonth);
       } else if (expMonth.value > 12) {
@@ -230,15 +224,15 @@ expYear.addEventListener("input", function expMonthValidation() {
     case onlyNumbersRegex.test(expYear.value):
       if (expYear.value.length > 2) {
         expYear.value = expYear.value.slice(0, 2);
-      }else if (expYear.value.length < 2) {
+      } else if (expYear.value.length < 2) {
         setErrorMessage(3, "must entre two numbers");
         redBorder(expYear);
-      }else{
+      } else {
         greenBorder(expYear);
-      printedExpYear.innerText = formatNumber(expYear.value, 2);
-      setSuccess(3);
+        printedExpYear.innerText = formatNumber(expYear.value, 2);
+        setSuccess(3);
       }
-      
+
       break;
 
     default:
@@ -256,16 +250,22 @@ cvc.addEventListener("input", function expMonthValidation() {
     case cvc.value == "":
       redBorder(cvc);
       setErrorMessage(4, "Can't be blank.");
+      printedCvc.textContent = '000'
       break;
 
     case !onlyNumbersRegex.test(cvc.value):
       redBorder(cvc);
       setErrorMessage(4, "Wrong format, numbers only.");
       break;
+    case onlyNumbersRegex.test(cvc.value):
 
-    case cvc.value.length > 3:
+    if( cvc.value.length > 3){
       cvc.value = cvc.value.slice(0, 3);
-      break;
+
+    }else{
+      printedCvc.innerText= formatNumber(cvc.value,3)
+    }
+    
 
     default:
       greenBorder(cvc);
@@ -273,13 +273,14 @@ cvc.addEventListener("input", function expMonthValidation() {
   }
 });
 
-
-
 function formatNumber(str, len) {
-  str = str + "0".repeat(len - str.length);
+  str = str + "0".repeat(len - str.length); // creats a string of chracter with a specified length 'len' and the rest characters not specified in the input are 0 in this string
   let arr = [];
   for (let i = 0; i <= str.length; i += 4) {
-    arr.push(str.slice(i, i + 4));
+    // loop over the string 'str' with 4 position jump at  every loop
+    arr.push(str.slice(i, i + 4)); //  cut the string str by a 4 character chuncs from the initial position to a specified one by the loop which is more then the initial by 4
+    //  push every 4 character to the array til i > str.length
   }
-  return arr.join(" ");
+
+  return arr.join(" "); // join the strings of the array 'arr' and set a space btw them
 }
